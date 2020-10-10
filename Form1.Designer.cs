4ba8da66de32d5854ecedc780b453f06f0ac6a36
dark angel <72620239+darkanjelmta@users.noreply.github.com>
2020-10-10 12:30:05 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace REGMTA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SetRegMTA = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SetRegMTA
+            // 
+            this.SetRegMTA.Font = new System.Drawing.Font("Acrom Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SetRegMTA.Location = new System.Drawing.Point(12, 103);
+            this.SetRegMTA.Name = "SetRegMTA";
+            this.SetRegMTA.Size = new System.Drawing.Size(328, 62);
+            this.SetRegMTA.TabIndex = 0;
+            this.SetRegMTA.Text = "Установить";
+            this.SetRegMTA.UseVisualStyleBackColor = true;
+            this.SetRegMTA.Click += new System.EventHandler(this.SetRegMTA_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Acrom Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Нажмите \"Установить\"";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Acrom Medium", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(52, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Скачано с Moon Studio";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 177);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SetRegMTA);
+            this.Name = "Form1";
+            this.Text = "Установка реестра | Moon Studio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SetRegMTA;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
